725efd2d61e350d20ebd1a37f1fd6515ee45f5bd
Phil Nash <philnash@gmail.com>
2015-02-16 12:02:52 +0000

    Updates and makes easier to run in development
    * Fixes tests
    * Allows for different test and dev environment variables
    * Uses Bundler to load dependencies
    * Uses Foreman to start application and load env in development
    * Tabs -> Spaces
    * 404 errors for incorrect IDs in routes
    * Fixes Rack issue with returning a header that is not a string

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # An SMS Powered Coffee Shop
 
-This is a simple Ruby and Sinatra app to allow you to run your ordering system (build with a coffeeshop in mind) from a Twilio SMS number. 
+This is a simple Ruby and Sinatra app to allow you to run your ordering system (build with a coffeeshop in mind) from a Twilio SMS number.
 
 ## Features
 
@@ -13,18 +13,36 @@ This is a simple Ruby and Sinatra app to allow you to run your ordering system (
 
 The SMSCoffeeShop is pretty much ready to go out of the box. You need to set the following Environment Variables to make it work:
 
-`DATABASE_URL` This is the URL used for database. If you deploy to Heroku it will be automagically set.
-`PUSHER_URL` This is the URL used for your Pusher app, so that the system can push the updates to the tablet device.
-`PUSHER_KEY` This is the Pusher Application Key so that the tablet app can subscribe to notifications of orders etc.
-`TWILIO_ACCOUNT_SID` and `TWILIO_AUTH_TOKEN` These are your Twilio credentials so that SMSCoffeeShop can use your Twilio account to send SMS messages.
-`TWILIO_NUMBER` This is the advertised number. It is the number your customer will send SMS messages *to* when ordering a coffee. SMSCoffeeShop needs to know this number so that it can send out completion notifications. Don't foreget to configure this number in your Twilio Dahsboard to `http://<your server/app name>/order`.
-`ENABLE_SMS` This should be set to `YES` if you want the system to actually send out SMS messages. This is useful for load and performance testing the application. On a typical Heroku instance it can handle over 1000 coffees a minute, so that should be fine. (Although not if SMS is enabled, as Twilio slightly increases response times as API calls are made on the main thread.)
-`SUPERVISOR_NUMBER` This is telephone number of the Twilio person at the event. If anything goes wrong, error messages will be sent to this number providing instructions on what you should do.
-
-You can create a copy of `.env.example` as `.env` to load these parameters locally and test.
+* `DATABASE_URL` This is the URL used for database. If you deploy to Heroku it will be automagically set.
+* `PUSHER_URL` This is the URL used for your Pusher app, so that the system can push the updates to the tablet device.
+* `PUSHER_KEY` This is the Pusher Application Key so that the tablet app can subscribe to notifications of orders etc.
+* `TWILIO_ACCOUNT_SID` and `TWILIO_AUTH_TOKEN` These are your Twilio credentials so that SMSCoffeeShop can use your Twilio account to send SMS messages.
+* `TWILIO_NUMBER` This is the advertised number. It is the number your customer will send SMS messages *to* when ordering a coffee. SMSCoffeeShop needs to know this number so that it can send out completion notifications. Don't foreget to configure this number in your Twilio Dahsboard to `http://<your server/app name>/order`.
+* `ENABLE_SMS` This should be set to `YES` if you want the system to actually send out SMS messages. This is useful for load and performance testing the application. On a typical Heroku instance it can handle over 1000 coffees a minute, so that should be fine. (Although not if SMS is enabled, as Twilio slightly increases response times as API calls are made on the main thread.)
+* `SUPERVISOR_NUMBER` This is telephone number of the Twilio person at the event. If anything goes wrong, error messages will be sent to this number providing instructions on what you should do.
 
 Finally, you need to configure the drinks you are serving at the coffee stand. This is a simple matter of editing the Hash in `order.rb`. The official name is how the system will reply to the user, and to the Barista. However it will match based on the values in `corruptions`, which should include all acceptable spellings (including the correct one).
 
 Deploy the app, and you're ready to go. Point a tablet at `http://<your server/app name>/coffee-shop-bar-manager-thingy-do-dar`, and the Baristas will see the ordering process.
 
+## Development
 
+To use in development, copy `.env.example` to `.env` and fill in your development credentials.
+
+Run the application with
+
+```shell
+$ bundle exec foreman start
+```
+
+The application will start on http://localhost:5000/.
+
+## Test
+
+To run the tests, make sure you have your credentials set in a `.env` file. If you want to use different credentials to development (i.e. a different DATABASE_URL) then you can create a `.env.test` file to override the `.env` file.
+
+Run the tests with
+
+```shell
+$ ruby shop_test.rb
+```
--- shop_test.rb
+++ shop_test.rb
@@ -1,8 +1,8 @@
-ENV['RACK_END'] = 'test'
+ENV['RACK_ENV'] = 'test'
 
 #Load environment variables from .env file...
 require 'dotenv'
-Dotenv.load
+Dotenv.load('.env.test', '.env')
 
 require './shop'
 require 'test/unit'
@@ -11,104 +11,104 @@ require 'rack/test'
 ENV['ENABLE_SMS'] = "NO"
 
 class ShopTest < Test::Unit::TestCase
-	include Rack::Test::Methods
+  include Rack::Test::Methods
   def setup
 
-		Customer.all.destroy!
-		Order.all.destroy!
-	end
+    Customer.all.destroy!
+    Order.all.destroy!
+  end
 
-	def app 
-		Sinatra::Application
-	end
+  def app
+    Sinatra::Application
+  end
 
   def test_it_should_wakeup
-	  get '/wakeup'
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-    assert_equal 'OK', last_response.body, "Wakeup Failed"	
-	end	
+    get '/wakeup'
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert_equal 'OK', last_response.body, "Wakeup Failed"
+  end
 
   def test_it_should_render_dashboard
     get '/coffee-shop-bar-manager-thingy-do-dar'
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?('SMS Coffee Shop'), "Body didn't include expected text: #{last_response.body}"
-	end
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?('SMS Coffee Shop'), "Body didn't include expected text: #{last_response.body}"
+  end
 
   def test_respond_to_help_sms
     post '/order', {:Body => "help", :From => "+2"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>Oh dear. We're going to give you a call"), "Last response not as expected. #{last_response.body}"
-	end
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>Oh dear. We're going to give you a call"), "Last response not as expected. #{last_response.body}"
+  end
 
-	def test_respond_to_valid_drink_sms
+  def test_respond_to_valid_drink_sms
     post '/order', {:Body => "tea", :From => "+1"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
     assert_not_nil last_response.headers['x-order-id']
-	end
+  end
 
   def test_invalid_order_is_rejected
     post '/order', {:Body => "banana", :From => "+7"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>Twilio's SMS Coffee Shop doesn't"), "Last Response not as expected. #{last_response.body}"
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>Twilio's SMS Coffee Shop doesn't"), "Last Response not as expected. #{last_response.body}"
     assert_not_nil last_response.headers['x-order-id']
-	end
+  end
 
   def test_reorder_invalid_drink_is_rejectd
     post '/order', {:Body => "tea", :From => "+6"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>Thanks for ordering a"), "Last Response not as expected. #{last_response.body}"
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>Thanks for ordering a"), "Last Response not as expected. #{last_response.body}"
     post '/order', {:Body => "banana", :From => "+6"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>We're still making you a "), "Last Response not as expected. #{last_response.body}"
-	end
-  
-	def test_reorder_valid_drink_is_rejectd
-    post '/order', {:Body => "tea", :From => "+5"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>Thanks for ordering a"), "Last Response not as expected. #{last_response.body}"
-    post '/order', {:Body => "tea", :From => "+5"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?("<Message>We're still making you a "), "Last Response not as expected. #{last_response.body}"
-	end
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>We're still making you a "), "Last Response not as expected. #{last_response.body}"
+  end
 
-	def test_fulfillment_works
+  def test_reorder_valid_drink_is_rejectd
+    post '/order', {:Body => "tea", :From => "+5"}
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>Thanks for ordering a"), "Last Response not as expected. #{last_response.body}"
+    post '/order', {:Body => "tea", :From => "+5"}
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?("<Message>We're still making you a "), "Last Response not as expected. #{last_response.body}"
+  end
+
+  def test_fulfillment_works
     post '/order', {:Body => "tea", :From => "+3"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
     order_id = last_response.headers['x-order-id']
 
-		post '/forfill/'+order_id.to_s
+    post '/forfill/'+order_id.to_s
     assert last_response.ok?, "Expected 200 on fulfillment but got #{last_response.status}"
-	end
+  end
 
-	def test_cancellation_works
+  def test_cancellation_works
     post '/order', {:Body => "tea", :From => "+4"}
-		assert last_response.ok?, "Expected 200 but got #{last_response.status}"
-		assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
+    assert last_response.ok?, "Expected 200 but got #{last_response.status}"
+    assert last_response.body.include?('<Message>Thanks for ordering'), "Last Response not as expected. #{last_response.body}"
     order_id = last_response.headers['x-order-id']
 
-		post '/cancel/'+order_id.to_s
+    post '/cancel/'+order_id.to_s
     assert last_response.ok?, "Expected 200 on fulfillment but got #{last_response.status}"
-	end
+  end
 
-	def test_cancel_invalid_string_order_works
+  def test_cancel_invalid_string_order_works
     post '/cancel/fredsentme'
-		assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
-	end
+    assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
+  end
 
-	def test_cancel_invalid_numeric_order_works
+  def test_cancel_invalid_numeric_order_works
     post '/cancel/12345678987'
-		assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
-	end
+    assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
+  end
 
-	def test_forfillment_invalid_string_order_works
+  def test_forfillment_invalid_string_order_works
     post '/forfill/fredsentme'
-		assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
-	end
+    assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
+  end
 
-	def test_forfillment_invalid_numeric_order_works
+  def test_forfillment_invalid_numeric_order_works
     post '/forfill/12345678987'
-		assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
-	end
+    assert !last_response.ok?, "Platform errored which it should not have done. #{last_response.status}"
+  end
 end
